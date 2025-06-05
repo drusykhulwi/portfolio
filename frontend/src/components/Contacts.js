7939ed4f86e29f826c96e8f6061eef5044e552d1
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-06-05 16:40:20 +0300

    Website components

---
+++ frontend/src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Contacts() {
+  return (
+    <div>
+      <Navbar/>
+      Contacts
+    </div>
+  )
+}
+
+export default Contacts
---
+++ frontend/src/components/Services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Services() {
+  return (
+    <div>
+      <Navbar/>
+      Services
+    </div>
+  )
+}
+
+export default Services
