63f91387788af093c2c7083b8af4f96ad1c50feb
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-06-07 17:35:14 +0300

    Added the roboto mono font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects';
-import Services from './components/Services'
-import Contacts from './components/Contacts'
-import Navbar from './components/Navbar';
+import Homepage from './Homepage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-primary">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="roboto-mono min-h-screen bg-primary">
+      <Homepage/>
+    </div>
   );
 }
