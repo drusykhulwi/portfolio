82c0d2cb0914766fc597c72373768fafe235250c
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-06-19 18:30:45 +0300

    Added the add and get about functions

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,32 @@ const db = mysql.createPool({
 }).promise();
 
 app.use(express.json());
+
+async function addAbout(name, bio, profileImage, cvURL) {
+    const connection = await db.getConnection();
+    try{
+        const [results] = await connection.query(
+            'INSERT INTO about (name, bio, profile_image, cv_url) VALUES (?, ?, ?, ?)',
+            [name, bio, profileImage, cvURL]
+        );
+        return results;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
+
+async function getAbout() {
+    const connection = await db.getConnection();
+    try {
+        const [results] = await connection.query('SELECT * FROM about');
+        return results;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        connection.release;
+    }
+}
